0e557fc233252ba7aed91da0b75b0977f409c63f
RamilAro <115680732+RamilAro@users.noreply.github.com>
2023-10-28 16:32:48 +0800

    Add files via upload

---
+++ MG-js.js
@@ -0,0 +1,14 @@
+document.addEventListener("DOMContentLoaded", function () {
+    const startButton = document.getElementById("start-button");
+    
+    startButton.addEventListener("click", function () {
+        // Navigate to the first region (first_region.html)
+        window.location.href = "first_region.html";
+    });
+});
+
+
+
+
+
+
